bd7ce646f7fc9f53f51c3c6e30f50a3b45f3ee4a
pr-estt <mcadamsrd@gmail.com>
2026-05-08 08:23:50 +0000

    feat: added crud logic

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 from app.models import Task
-from app.schemas import TaskCreate
+from app.schemas import TaskCreate, TaskUpdate
 
 def get_tasks(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Task).offset(skip).limit(limit).all()
@@ -13,6 +13,15 @@ def create_task(db: Session, task: TaskCreate):
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
+    return db_task
+
+def update_task(db: Session, task_id: int, task: TaskUpdate):
+    db_task = get_task(db=db, task_id=task_id)
+    if db_task:
+        for key, value in task.dict().items():
+            setattr(db_task, key, value)
+        db.commit()
+        db.refresh(db_task)
     return db_task
 
 def delete_task(db: Session, task_id: int):
