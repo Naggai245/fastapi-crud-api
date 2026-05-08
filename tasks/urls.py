39981d7dbc457faadd84c9420019fea3311043e7
pr-estt <mcadamsrd@gmail.com>
2026-05-08 08:19:58 +0000

    fix: removed unrelated django files

--- tasks/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import TaskListView, TaskDetailView
-urlpatterns = [
-    path('', TaskListView.as_view(), name='task-list'),
-    path('<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
-]
